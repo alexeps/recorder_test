2b849b6a15a461537282bf1fb33eeeaf04642afd
Alexey Levchenko <alexey.levchenko@easternpeak.com>
2017-10-03 12:09:44 +0300

    Initial Commit

---
+++ audio_recorderTests/audio_recorderTests.swift
@@ -0,0 +1,36 @@
+//
+//  audio_recorderTests.swift
+//  audio_recorderTests
+//
+//  Created by EasternPeak on 10/3/17.
+//  Copyright © 2017 EasternPeak. All rights reserved.
+//
+
+import XCTest
+@testable import audio_recorder
+
+class audio_recorderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
